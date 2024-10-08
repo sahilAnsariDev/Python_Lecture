1b6f64ee3cfa96c7a7cc634b8955d26fb64e49b5
unknown <sahil.ansari.dev@gmail.com>
2024-10-08 09:45:50 -0700

    we learn nonprimitive datatype and inputs to take all datatype

---
+++ inputs.py
@@ -0,0 +1,19 @@
+#taking values from users
+
+
+#string
+name = input("enter your name:")
+print(name)
+
+
+#int
+number = int(input("enter your number:"))
+print(number)
+
+#array
+numbers = list(map(int, input().split()))
+print(numbers)
+
+#list
+objects = list(input())
+print(objects)
--- nonprimitive.py
+++ nonprimitive.py
@@ -1,1 +1,42 @@
-print("hello")
+#non primitive datatype
+
+#string
+
+name = "kaif"
+
+print(name)
+
+#list
+
+
+numberofobject = ["string",12,True,3.00]
+
+print("list datatype:-",numberofobject)
+
+#array
+
+IntArray = [1,2,3,4,5]
+FloatArray = [1.0,2.2,3.3]
+
+print(IntArray)
+print(FloatArray)
+
+
+#sets
+wholenumber = {1,1,2,3,4}
+print(wholenumber)
+
+#dictionary
+person = {
+        "name":"tufail",
+        "age":12,
+        "height":"5'2",
+        "weight":75,
+        }
+
+print("dictionary of person",person)
+
+#touple
+
+person1 = ("tufail","khan",)
+print(person1)
